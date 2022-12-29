c42fe484591afcff34fe84566d96e8d4c2fcbd43
Maung Htike <maunghtike1999@gmail.com>
2022-12-29 14:21:31 -0800

    fixed issue where home timeline links did not work

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -22,12 +22,12 @@ export default function Home() {
             <h3 className="mt-8 mb-16 text-center text-neutral text-xl font-bold" >View some of our already built timelines for some inspiration!</h3>
 
             <div className="card w-2/3 bg-base-100 shadow-xl mx-auto my-12">
-                <figure><img src="https://i.imgur.com/mR1LpPK.png" alt="Hiking adventure timeline" /></figure>
+                <figure><img src="https://lh3.google.com/u/0/d/1iNI7VpXgYIofgYaP85yA-MGebEwWIozq=w2560-h1264-iv1" alt="Fun Memories timeline" /></figure>
                 <div className="card-body">
-                    <h2 className="card-title">Timeline of Hiking Adventures</h2>
-                    <p>A personal timeline of hiking adventures.</p>
+                    <h2 className="card-title">Timeline of Fun Memories</h2>
+                    <p>A personal timeline of some of my recent fun moments.</p>
                     <div className="card-actions justify-end">
-                        <Link to={"/timeline/62a6c00e9d2bb28ed6147db7"}>
+                        <Link to={"/timeline/63ae0e7479fcfed0b22bd9b6"}>
                             <button className="btn btn-primary">View Timeline</button>
                         </Link>
                     </div>
@@ -40,7 +40,7 @@ export default function Home() {
                     <h2 className="card-title">Timeline of Star Wars Movies</h2>
                     <p>All the Star Wars movies in Earth-time chronological order.</p>
                     <div className="card-actions justify-end">
-                        <Link to={"/timeline/62a6d62f57bb816e1730b731"}>
+                        <Link to={"/timeline/63adfb9883e150456fcd10cd"}>
                             <button className="btn btn-primary">View Timeline</button>
                         </Link>
                     </div>
@@ -53,7 +53,7 @@ export default function Home() {
                     <h2 className="card-title">Timeline of US Historical Events</h2>
                     <p>Major historical events that occurred in the United States.</p>
                     <div className="card-actions justify-end">
-                        <Link to={"/timeline/62a6dae757bb816e1730b762"}>
+                        <Link to={"/timeline/63adfb9883e150456fcd10cf"}>
                             <button className="btn btn-primary">View Timeline</button>
                         </Link>
                     </div>
